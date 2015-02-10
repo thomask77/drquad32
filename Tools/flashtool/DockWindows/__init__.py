526c6ceb6d53cb1106c5fdf290e659fc586b20a1
Thomas Kindler <mail_git@t-kindler.de>
2015-02-10 23:15:44 +0100

    Removed old PyQt-Tool

--- Tools/flashtool/DockWindows/__init__.py
+++
@@ -1,9 +0,0 @@
-import ConnectionWindow
-import ConsoleWindow
-import UpdateWindow
-import GLWindow
-
-ConnectionWindow = ConnectionWindow.ConnectionWindow
-ConsoleWindow = ConsoleWindow.ConsoleWindow
-UpdateWindow = UpdateWindow.UpdateWindow
-GLWindow = GLWindow.GLWindow
