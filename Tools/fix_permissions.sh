0712b9db6a495ca81254b3e35fa678331bafec98
Thomas Kindler <mail_git@t-kindler.de>
2015-02-18 16:54:16 +0100

    fix_permissions.py script remove annonying executable bit from files under cygwin

---
+++ Tools/fix_permissions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# fix permissions on cygwin
+#
+find . -type f -exec chmod 644 {} +
+chmod 755 Tools/fix_permissions.sh
+chmod 755 Tools/add_version_info/add_version_info.py
+chmod 755 Tools/tablegen/{gamma_tab.py,ws2812_tab.py}
+chmod 755 Tools/st-link/*.exe
+chmod 755 Tools/st-link/*.dll
